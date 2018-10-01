fea169ee063bbb057f929a916a926de525f6b891
Qingcai <qingcai518@gmail.com>
2018-10-01 11:06:30 +0900

    call api to download file

--- src/main/java/jp/bctech/common/ExcelUtility.java
+++ src/main/java/jp/bctech/common/ExcelUtility.java
@@ -8,7 +8,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import java.io.*;
 
 public class ExcelUtility {
-	public void create() {
+	public static void create() {
 		Workbook workbook = new HSSFWorkbook();
 		Sheet sheet = workbook.createSheet("test");
 		
@@ -33,5 +33,28 @@ public class ExcelUtility {
 				System.out.println(e.toString());
 			}
 		}
+	}
+	
+	public static void createExcel() {
+		Workbook workbook = new HSSFWorkbook();
+		Sheet sheet = workbook.createSheet("content");
+		
+		Row row1 = sheet.createRow(1);
+		Row row4 = sheet.createRow(4);
+		
+		FileOutputStream output = null;
+		try {
+			output = new FileOutputStream("content.xls");
+			workbook.write(output);
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		} finally {
+			try {
+				output.close();
+			} catch (IOException e) {
+				System.out.println(e.toString());
+			}
+		}
+				
 	}
 }
--- src/main/java/jp/bctech/controller/TestController.java
+++ src/main/java/jp/bctech/controller/TestController.java
@@ -8,19 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import jp.bctech.common.ExcelUtility;
+
 @Controller
 @RequestMapping("/test")
 public class TestController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String test(Model model) {
 		model.addAttribute("msg", "サンプルメッセージ");
+		
+		ExcelUtility.create();
+		
 		return "test";
-	}
-	
-	@RequestMapping(value = "download", method = RequestMethod.POST)
-	public ModelAndView download(Map<String, Object> model) throws Exception {
-		ModelAndView mav = new ModelAndView(new ExcelBuilder());
-		mav.addObject("fileName", "testExcel" + ".xls");
-		return mav;
 	}
 }

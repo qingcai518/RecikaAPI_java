6b83ab378c66e3f9680dad02f7399ad0c8fcde7f
Qingcai <qingcai518@gmail.com>
2018-10-01 13:10:35 +0900

    formater import

--- src/main/java/jp/bctech/controller/CalendarController.java
+++ src/main/java/jp/bctech/controller/CalendarController.java
@@ -1,20 +1,13 @@
 package jp.bctech.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class CalendarController {
-	
+
 	@RequestMapping(path = "/calendar", method = RequestMethod.GET)
 	public String index(Model model) {
 		return "test";
--- src/main/java/jp/bctech/controller/EventController.java
+++ src/main/java/jp/bctech/controller/EventController.java
@@ -1,13 +1,14 @@
 package jp.bctech.controller;
 
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jp.bctech.entity.Event;
 
 @RestController
@@ -18,21 +19,21 @@ public class EventController {
 		String jsonMsg = null;
 		try {
 			List<Event> events = new ArrayList<Event>();
-			
+
 			Event e1 = new Event();
 			e1.setTitle("first event");
 			e1.setStart("2018-10-01");
 			events.add(e1);
-			
+
 			Event e2 = new Event();
 			e2.setTitle("second event");
 			e2.setStart("2018-10-11");
 			e2.setEnd("2018-10-12");
 			events.add(e2);
-			
+
 			ObjectMapper mapper = new ObjectMapper();
 			jsonMsg = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(events);
-			
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}

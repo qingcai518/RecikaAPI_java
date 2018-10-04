2bf43db63397a1d1076429c0986a6e42bb994775
Qingcai <qingcai518@gmail.com>
2018-10-04 14:59:05 +0900

    remove unuse code

--- src/main/java/jp/bctech/controller/UserController.java
+++ src/main/java/jp/bctech/controller/UserController.java
@@ -1,16 +1,15 @@
 package jp.bctech.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.*;
 
 import jp.bctech.entity.User;
 import jp.bctech.service.UserService;
@@ -20,23 +19,23 @@ import jp.bctech.service.UserService;
 public class UserController {
 	@Autowired
 	UserService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public List<User> fetchAll() {
 		return service.fetchAll();
 	}
-	
-	@RequestMapping(method = RequestMethod.GET, value="{id}")
+
+	@RequestMapping(method = RequestMethod.GET, value = "{id}")
 	public Optional<User> fetch(@PathVariable("id") Long id) {
 		return service.fetch(id);
 	}
-	
-	@RequestMapping(method=RequestMethod.PUT, value="{id}")
+
+	@RequestMapping(method = RequestMethod.PUT, value = "{id}")
 	public User createUser(@Validated @RequestBody User user) {
 		return service.save(user);
 	}
-	
-	@RequestMapping(method=RequestMethod.DELETE, value="{id}")
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "{id}")
 	public void deleteUser(@PathVariable("id") Long id) {
 		service.delete(id);
 	}
